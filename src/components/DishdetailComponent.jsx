5ac2d7a0829fa10a854443a8589999fae2f77f1f
Milan <072bex426.milan@pcampus.edu.np>
2018-11-05 09:27:46 +0545

    Third assignment

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -1,7 +1,118 @@
-import React from 'react';
-import {Card,CardImg,CardText,CardTitle,CardBody,Breadcrumb,BreadcrumbItem} from "reactstrap"
+import React,{Component} from 'react';
+import {Card,CardImg,CardText,CardTitle,CardBody,Breadcrumb,BreadcrumbItem, Modal,ModalBody,ModalHeader,Button,Row,Col,Label} from "reactstrap"
+import 'font-awesome/css/font-awesome.min.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import {Link} from 'react-router-dom'
+import {Control,LocalForm,Errors} from 'react-redux-form'
+
+const required =(val) =>val&&val.length;
+const maxLength = (len)=>(val)=>!(val)||(val.length<=len)
+const minLength = (len)=>(val)=>(val)&&(val.length>=len)
+
+
+
+class CommentForm extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            modalOpen:false
+        }
+    }
+
+    handleToggle=()=>
+    {
+        this.setState(
+            {
+                modalOpen:!this.state.modalOpen
+            }
+        )
+    }
+
+    submitHandle=(values)=>
+    {
+        this.handleToggle();
+        
+    }
+    render() { 
+        return ( 
+            <React.Fragment>
+            <Button className="bg-white text-dark" onClick={this.handleToggle}><i className="fa fa-pencil fa-lg"></i>{' '}Submit Comment</Button>
+            <Modal isOpen={this.state.modalOpen} toggle={this.handleToggle}>
+                <ModalHeader toggle={this.handleToggle}>
+                    Submit Comment
+                </ModalHeader>
+                <ModalBody>
+                    <LocalForm onSubmit={(value)=>this.submitHandle(value)}>
+                    <Row className="form-group">
+                                <Label htmlFor="rating" md={4}>Rating</Label>
+                                <Col md={12}>
+                                    <Control.select model=".rating" id="rating" name="rating"
+                                        className="form-control"
+                                         >
+                                         <option>1</option>
+                                         <option>2</option>
+                                         <option>3</option>
+                                         <option>4</option>
+                                         <option>5</option>
+                                    </Control.select>
+                                </Col>
+                    </Row>
+                    <Row className="form-group">
+                                <Label htmlFor="author" md={4}>Your Name</Label>
+                                <Col md={12}>
+                                    <Control.text model=".author" id="author" name="author"
+                                        placeholder="Your Name"
+                                        className="form-control"
+                                        validators={{
+                                            minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                         />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        messages={{
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                     />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="comment" md={4}>Comment</Label>
+                                <Col md={12}>
+                                    <Control.textarea model=".comment" id="comment" name="comment"
+                                        className="form-control"
+                                        validators={{
+                                            required
+                                        }}
+                                        rows="6"
+                                         />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".comment"
+                                        show="touched"
+                                        messages={{
+                                            required:'Comment Required'
+                                        }}
+                                     />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                            <Col >
+                                <Button type="submit" color="primary" >Submit</Button>
+                            </Col>
+                            </Row>
+                    </LocalForm>
+                </ModalBody>
+            </Modal>
+            </React.Fragment>
+         );
+    }
+}
+ 
+
 
 
 const Dishdetail = (props)=>{
@@ -24,7 +135,7 @@ function formatDate(date)
 }
 
 
-const renderComments =(comments) =>
+const RenderComments =(comments) =>
     {
 
         if (comments!=null)
@@ -43,7 +154,9 @@ const renderComments =(comments) =>
             return(
                 <ul className="list-unstyled">
                 {com}
+                <CommentForm />
                 </ul>
+                
             )
         }
         else{
@@ -79,7 +192,7 @@ const renderDish=(dish,comments)=>
             </div>
             <div className="col-12 col-md-5 m-1" >
             <h4>Comments</h4>
-            {renderComments(comments)}
+            {RenderComments(comments)}
             </div>
             </div>
             </div >
