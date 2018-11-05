b59025c2bfee0864efd918622c8cda5d37c04dfa
Milan <072bex426.milan@pcampus.edu.np>
2018-11-05 12:44:53 +0545

    react redux global form with store

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,7 +11,7 @@ import Contact from './ContactComponent'
 import {Switch,Route,Redirect,withRouter} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {addComment,fetchDishes} from '../redux/ActionCreators'
-
+import {actions } from 'react-redux-form'
 
  const mapStateToProps = state=>
 {
@@ -28,7 +28,8 @@ const mapDispatchToProps = (dispatch)=>
   return (
     {
       addComment:(dishId,rating,author,comment)=>dispatch(addComment(dishId,rating,author,comment)),
-      fetchDishes:()=>{dispatch(fetchDishes())}
+      fetchDishes:()=>{dispatch(fetchDishes())},
+      resetFeedbackForm:()=>{dispatch(actions.reset('feedback'))}
     }
   )
 }
@@ -75,7 +76,7 @@ class Main extends Component {
       <Route path='/aboutus' component={()=><About leaders={this.props.leaders}/>} />
       <Route exact path='/menu' component={()=><Menu dishes={this.props.dishes}/>} />
       <Route path='/menu/:dishId' component={DishWithId}/>
-      <Route exact path='/contactus' component={Contact} />
+      <Route exact path='/contactus' component={()=><Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
       <Redirect to="/home" />
     </Switch>
     <Footer />
